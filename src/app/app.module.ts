5af84a8daccde5f94d2c4ae7dcb119753332ee87
JohnErikcObando <Jeobando11@hotmail.com>
2021-04-05 16:41:01 -0500

    Tipos de Graficos Charts.js

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,23 @@ import { ChartsModule } from 'ng2-charts';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LineaComponent } from './components/linea/linea.component';
+import { BarraComponent } from './components/barra/barra.component';
+import { DonaComponent } from './components/dona/dona.component';
+import { RadarComponent } from './components/radar/radar.component';
+import { PieComponent } from './components/pie/pie.component';
 
 //char.js Graficos
 // import { ChartsModule } from 'ng2-charts';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LineaComponent,
+    BarraComponent,
+    DonaComponent,
+    RadarComponent,
+    PieComponent
   ],
   imports: [
     BrowserModule,
